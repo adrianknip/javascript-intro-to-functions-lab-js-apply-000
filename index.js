f8dd1629e1699e0c9f96da8fcb803b854a9d63d4
nip.adrian@gmail.com Nip <nip.adrian@gmail.com>
2018-06-01 04:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   string === string.toLowerCase()
   return "I can't hear you!"
-  string === string.toUpperCase
+  string === string.toUpperCase()
   return "YES INDEED!"
 }
 
